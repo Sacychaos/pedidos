d151b49e817a16af5843927a19635f09751cc518
Raul Lisboa <raullisboasilva@gmail.com>
2023-09-10 12:51:21 -0300

    migration paara criar user admin

---
+++ database/migrations/2023_09_10_124135_add_admin_user_and_adm_sector.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Sector;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Verifica se o setor "Admin" já existe
+        $adminSector = Sector::where('name', 'Admin')->first();
+
+        // Se o setor "Admin" não existe, cria-o
+        $admSector = Sector::create([
+            'name' => 'Admin',
+            'cancelled' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Cria o usuário administrador e associa ao setor "Admin"
+        User::create([
+            'name' => 'Admin',
+            'username' => 'admin', // Substitua pelo nome de usuário desejado
+            'password' => bcrypt('teste'),
+            'is_admin' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'cancelled' => false,
+            'sector_id' => $adminSector->id,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
