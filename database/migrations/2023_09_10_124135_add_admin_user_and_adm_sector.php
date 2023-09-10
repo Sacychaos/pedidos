42e899ab415512adfd906a46ef03b673821b8076
Raul Lisboa <raullisboasilva@gmail.com>
2023-09-10 12:52:20 -0300

    migrate user admin

--- database/migrations/2023_09_10_124135_add_admin_user_and_adm_sector.php
+++ database/migrations/2023_09_10_124135_add_admin_user_and_adm_sector.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Sector;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
         $adminSector = Sector::where('name', 'Admin')->first();
 
         // Se o setor "Admin" não existe, cria-o
-        $admSector = Sector::create([
+        $adminSector = Sector::create([
             'name' => 'Admin',
             'cancelled' => false,
             'created_at' => now(),
